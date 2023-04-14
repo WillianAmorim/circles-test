bf0f1b9a4ee49d726da773090ebdac3d62f81b24
Willian <willian.amorim1@outlook.com>
2023-04-13 21:47:29 -0300

    criado função createCircles e state de circles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [circles, setCircles] = useState([]);
 
   //UseEffect executa handleclick cada vez que um clique é feito
   useEffect(() => {
@@ -11,8 +12,24 @@ function App() {
     };
   });
 
-  const handleClick = (event) => {
+  const createCircles = (event) => {
+    const section = document.querySelector('section')
+    const novoElemento = document.createElement("div");
+    novoElemento.style.position = "absolute";
     
+    novoElemento.style.left = event.clientX + "px";
+    novoElemento.style.top = event.clientY + "px";
+    novoElemento.style.width = '50px'
+    novoElemento.style.height = '50px'
+    novoElemento.style.backgroundColor = 'black'
+    novoElemento.style.borderRadius = '25px'
+    section.appendChild(novoElemento);
+
+    setCircles(prevCircles => [...prevCircles, novoElemento]);
+  }
+
+  const handleClick = (event) => {
+    createCircles(event)
   }
 
 
